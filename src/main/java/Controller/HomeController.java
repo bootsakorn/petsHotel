505a5d35a38f2827d8aaa9630ed2c9c0e9eaa0d1
sasithorn <sasithorn.sai@ku.th>
2018-11-13 21:23:44 +0700

    update directory file

---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController {
+
+    @FXML
+    protected Button btn;
+
+    @FXML
+    protected void number(ActionEvent e){
+        System.out.println("1");
+    }
+}
---
+++ src/test/java/Controller/HomeControllerTest.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    public void test(){}
+}
