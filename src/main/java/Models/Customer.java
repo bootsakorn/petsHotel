1ecd6b2918395f8eafa0f4757a77e8fa146c6c32
sasithorn <sasithorn.sai@ku.th>
2018-11-14 12:36:14 +0700

    update class customer and pets

--- src/main/java/Models/Customer.java
+++ src/main/java/Models/Customer.java
@@ -1,4 +1,22 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private ArrayList<Pets> pets;
+
+    public Customer(String firstName, String lastName, String address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pets = new ArrayList<>();
+    }
+
+    public void addPets(Pets newPets){
+        this.pets.add(newPets);
+    }
+
+
 }
--- src/main/java/Models/Pets.java
+++ src/main/java/Models/Pets.java
@@ -1,4 +1,64 @@
 package Models;
 
 public class Pets {
+    private String name;
+    private String sex;
+    private int age;
+    private String breed;
+    private String disease;
+    private String allergy;
+    private String species;
+
+    public Pets(String name, String sex, int age, String breed, String disease, String allergy, String species){
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.breed = breed;
+        this.disease = disease;
+        this.allergy = allergy;
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public String getDisease() {
+        return disease;
+    }
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+    public String getAllergy() {
+        return allergy;
+    }
+    public void setAllergy(String allergy) {
+        this.allergy = allergy;
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
 }
