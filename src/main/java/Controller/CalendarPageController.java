ca4a903f8c94d61bf3484be3d1af49c6d53bddc1
sasithorn <sasithorn.sai@ku.th>
2018-11-29 22:27:38 +0700

    edit fx:id in fxml files

--- src/main/java/Controller/CalendarPageController.java
+++ src/main/java/Controller/CalendarPageController.java
@@ -1,7 +1,12 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
 
 public class CalendarPageController extends PageSwitchController{
+    @FXML protected GridPane calendarTable;
+    @FXML protected ListView listView;
 
 }
