e4ce9fe32c56fcf535708e7b011fe9633ac1fc73
sasithorn <sasithorn.sai@ku.th>
2018-12-20 20:40:33 +0700

    add some cucumber test

--- src/main/resources/feature/checkOut.feature
+++ src/main/resources/feature/checkOut.feature
@@ -1,19 +1,15 @@
-//เชคเอ้าท์ให้ลูกค้า
-
 Feature: Check out
     As a Cashier
     I want to check out.
 
-Background:
-
 Scenario: choose an appointment number and click check out
-    Given a customer "Mr.B" check out with appointment number 3
-    When I click appointment number 3
+    Given a customer "Mr.B" check out with appointment number 26
+    When I click appointment number 26
     And I check out
-    Then system delete appointment number 3
+    Then system edit status in checkout number 10 to true
 
-Background:
-Scenario: no have an appointment number and click check out
-    Given a customer "Mr.B" check out witout appointment number
-    When I click I check out
-    Then I can't check out
+#Background:
+#Scenario: no have an appointment number and click check out
+#    Given a customer "Mr.B" check out witout appointment number
+#    When I click I check out
+#    Then I can't check out
---
+++ src/test/java/StepDef.java
@@ -0,0 +1,61 @@
+import Controller.dataController.CheckoutDataController;
+import Controller.dataController.DataController;
+import Models.CheckOut;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+public class StepDef {
+    private CheckoutDataController cdc;
+    private DataController dc;
+    private ArrayList<CheckOut> list;
+    private CheckOut checkOut;
+
+    @Before
+    public void setup() {
+        try {
+            dc = new DataController();
+            cdc = new CheckoutDataController();
+            list = dc.getCheckOuts();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Given("a customer (.*) check out with appointment number (\\d+)")
+    public void givenCheckout(String name, int id) {
+        for (CheckOut checkOut:list){
+            if (checkOut.getAppointment_bill_id() == id){
+                this.checkOut = checkOut;
+            }
+        }
+    }
+    @When("I click appointment number (\\d+)")
+    public void whenCheckOut(int id){
+
+    }
+
+    @And("I check out")
+    public void andCheckOut(){
+
+    }
+
+    @Then("system edit status in checkout number (\\d+) to (.*)")
+    public void thenCheckOut(int id, String bl){
+        cdc.editStatus(id,1);
+        dc.getData();
+        list = dc.getCheckOuts();
+        boolean status = true;
+        for (CheckOut checkOut:list){
+            if (checkOut.getId() == id){
+               status = checkOut.isStatus();
+            }
+        }
+        cdc.editStatus(id,0);
+        assertEquals(Boolean.valueOf(bl), status);
+    }
+}
